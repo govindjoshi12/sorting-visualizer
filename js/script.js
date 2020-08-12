d651e074e29fe1784e7aecbaa0699349794ad712
Govind <joshi.govind12@gmail.com>
2020-08-12 06:16:51 -0500

    added other basic sorts

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 let array = [];
 const ARRAY_SIZE = 20;
-const MIN_NUM = 5;
+const MIN_NUM = 35;
 const MAX_NUM = 1000;
-const ANIM_SPEED = 50;
+const ANIM_SPEED = 100;
 var sorter = null;
 
 window.onload = initialize;
@@ -61,6 +61,7 @@ function getRandomInt(min, max) {
 }
 
 function sort(sortFunction) {
+    removeSortedClass();
     sorter = sortFunction;
     //disableButtons(true);
     animate();
@@ -68,6 +69,10 @@ function sort(sortFunction) {
 }
 
 function animate() {
+    //Animate must also be an async func
+    //Since await keyword only applies to
+    //"Promise" objects, and all async functions
+    //return a promise
     if(!sorter.next().done) {
         setTimeout(animate, ANIM_SPEED);
     }
@@ -77,6 +82,13 @@ function disableButtons(bool) {
     let buttons = document.getElementsByClassName("button");
     for(let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = bool;
+    }
+}
+
+function removeSortedClass() {
+    let barArr = document.getElementById("bar-container").childNodes;
+    for(let i = 0; i < barArr.length; i++) {
+        barArr[i].classList.remove("sorted-bar");
     }
 }
 
@@ -104,42 +116,81 @@ function* selectionSort() {
  
         barContainer.insertBefore(barArr[i], barArr[min_idx]);
         barContainer.insertBefore(barArr[min_idx], barArr[i]);
-        barArr[i].style.backgroundColor = "green"; 
+        barArr[i].classList.add("sorted-bar");
         yield;
     }
-    barArr[array.length - 1].style.backgroundColor = "green"; 
+    barArr[array.length - 1].classList.add("sorted-bar");
     disableButtons(false);
 }
 //Interrupting sort (by clicking randomize) doesn't stop animation, even though
 //array is still randomized...?
 
-function insertionSort() {
-    // let barContainer = document.getElementById("bar-container");
-    // let barArr = barContainer.childNodes;
+function* insertionSort() {
+    disableButtons(true);
+    let barContainer = document.getElementById("bar-container");
+    let barArr = barContainer.childNodes;
     for(let i = 1; i < array.length; i++) {
+        if(i + 1 < array.length)
+            barArr[i+1].classList.add("checking-bar");
         let currIdx = i;
         for(; array[currIdx] < array[currIdx - 1] && currIdx > 0; currIdx--) {
             let temp = array[currIdx - 1];
             array[currIdx - 1] = array[currIdx];
             array[currIdx] = temp;
+
+            barArr[currIdx].classList.add("swapping-bar");
+            barArr[currIdx-1].classList.add("swapping-bar");
+            yield;
+            barContainer.insertBefore(barArr[currIdx], barArr[currIdx-1]);
+            barContainer.insertBefore(barArr[currIdx-1], barArr[currIdx]);
+            yield;
+            barArr[currIdx].classList.remove("swapping-bar");
+            barArr[currIdx-1].classList.remove("swapping-bar");
+        }
+        if(i + 1 < array.length) {
+            yield;
+            barArr[i+1].classList.remove("checking-bar");
         }
     }
-    renderVisualizer();
-    console.log(array);
+
+    for(let i = 0; i < barArr.length; i++) {
+        barArr[i].classList.add("sorted-bar");
+        yield;
+    }
+    disableButtons(false);
 }
 
-function bubbleSort() {
+function* bubbleSort() {
+    disableButtons(true);
+    let barContainer = document.getElementById("bar-container");
+    let barArr = barContainer.childNodes;
     for(let i = 0; i < array.length - 1; i++) {
         //After each iteration, i elements from the end of the array will be sorted
-        for(let j = 0; j < array.length - i - 1; j++) {
+        let j = 0;
+        for(; j < array.length - i - 1; j++) {
+            barArr[j].classList.add("checking-bar");
+            barArr[j+1].classList.add("checking-bar");
+            yield;
             //Only swap if curr element is greater than next element
             if(array[j] > array[j+1]) {
                 let temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = temp;
+
+                barContainer.insertBefore(barArr[j], barArr[j+1]);
+                barContainer.insertBefore(barArr[j+1], barArr[j]);
+
+                barArr[j].classList.add("swapping-bar");
+                barArr[j+1].classList.add("swapping-bar");
+                yield;
+                barArr[j].classList.remove("swapping-bar");
+                barArr[j+1].classList.remove("swapping-bar");
             }
+            barArr[j].classList.remove("checking-bar");
+            barArr[j+1].classList.remove("checking-bar");
         }
+       barArr[j].classList.add("sorted-bar");
     }
-    renderVisualizer();
-    console.log(array);
+    barArr[0].classList.add("sorted-bar");
+    disableButtons(false);
 }
